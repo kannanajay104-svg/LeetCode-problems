b0698108e0ad5441d10dd155f99bbd6e3aa0b1b5
AJAY KANNAN R <kannanajay104@gmail.com>
2026-09-07 21:18:05 +0530

    Time: 239 ms (24.43%), Space: 55 MB (38.93%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+
+        lis = []
+
+        for _, h in envelopes:
+            left = 0
+            right = len(lis)
+
+            while left < right:
+                mid = (left + right) // 2
+
+                if lis[mid] < h:
+                    left = mid + 1
+                else:
+                    right = mid
+
+            if left == len(lis):
+                lis.append(h)
+            else:
+                lis[left] = h
+
+        return len(lis)
